dcf50cb5d2cca88468677d964f0dfbad4836f0a3
Michał Porażko <121014407+MichalPorazko@users.noreply.github.com>
2026-07-24 14:13:03 +0200

    Make 2-bit Vedic base case self-contained

--- Multipliers/Vedic Multiplier/Vedic_mult_2bit.v
+++ Multipliers/Vedic Multiplier/Vedic_mult_2bit.v
@@ -1,18 +1,24 @@
-module vedic_mult_2bit(a,b,out);
-    input [1:0]a,b;
-    output [3:0]out;
+module vedic_mult_2bit(
+    input  wire [1:0] a,
+    input  wire [1:0] b,
+    output wire [3:0] out
+);
 
-    wire w1,w2,w3;
-    wire c1;
+    wire cross_0;
+    wire cross_1;
+    wire high_pp;
+    wire carry_1;
 
     assign out[0] = a[0] & b[0];
 
-    assign w1 = a[1] & b[0];
-    assign w2 = a[0] & b[1];
-    assign w3 = a[1] & b[1];
+    assign cross_0 = a[1] & b[0];
+    assign cross_1 = a[0] & b[1];
+    assign high_pp = a[1] & b[1];
 
-    half_adder h1(w1,w2,out[1],c1);
-    half_adder h2(c1,w3,out[2],out[3]);
+    assign out[1] = cross_0 ^ cross_1;
+    assign carry_1 = cross_0 & cross_1;
 
+    assign out[2] = carry_1 ^ high_pp;
+    assign out[3] = carry_1 & high_pp;
 
 endmodule
